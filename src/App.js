a99c5ce6d531b58de1ce4760716f42464f4c4051
Jaime Kumar <jaimelacree@gmail.com>
2023-05-23 20:33:43 +0100

    adding todo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Link from './Link';
 import { v4 as uuidv4 } from 'uuid';
 import trackLogo from './track-logo.png'
 import cryptickleLogo from './cryptickle-logo.png'
+import togoLogo from './todoLogo.png'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <div className="row">
           <Link id={uuidv4()} link='https://funny-empanada-4680e6.netlify.app/' logo={trackLogo} title='Track' />
           <Link id={uuidv4()} link='https://scintillating-tulumba-084a2f.netlify.app/' logo={cryptickleLogo} title='Cryptickle' />
+          <Link id={uuidv4()} link='https://unrivaled-panda-64a9c1.netlify.app/' logo={todoLogo} title='Weekly Todoz' />
         </div>
       </div>
     </div>
